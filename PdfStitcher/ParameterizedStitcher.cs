2e7f6fbe126407010680616b48d54a058c176c1e
Matt Frederick <mfrederick91@gmail.com>
2020-04-21 09:53:51 -0400

    Created parameterized start; removed Inventor functionality for now.

---
+++ PdfStitcher/ParameterizedStitcher.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PdfSharp.Pdf;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+
+namespace PdfStitcher
+{
+    public class ParameterizedStitcher
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No parameters supplied; type 'help' for the list of switches.");
+                return;
+            }
+            if (args[0].ToLower() == "help")
+            {                
+                Console.WriteLine("-t=? Type of document to stitch (Arguments: pdf/inventor)");
+                Console.WriteLine("-r=? Degrees of rotation to be applied to documents: must be a multiple of 90 (Default 0)");
+                Console.WriteLine("-o=? Origin directory to search (Default is current directory)");
+                Console.WriteLine("-c=? Search sub folders within origin directory? (Default true)");
+                Console.WriteLine("-d=? Destination directory for output pdf (Default if current directory)");
+                Console.WriteLine("-n=? Name of merged file (Default is 'MERGED_DATETIME')");                
+                Console.WriteLine("-f={?} Files to be merged seperated by a colon ':' (Default is all documents of selected type in orgin directory)");
+                Console.WriteLine("-x=? Regex string to be parsed to get files to merge. (Takes priority over a list of files)");
+                Console.WriteLine("-s=? Show the PDF after creation. (Default true)");
+                return;
+            }
+            else
+            {
+                StitcherBuilder stitcherBuilder = new StitcherBuilder();
+                foreach (string argument in args)
+                {
+                    switch (argument[1])
+                    {
+                        case 't':
+                            stitcherBuilder.Type = StitcherBuilder.GetType(argument.Substring(3));
+                            break;
+                        case 'r':
+                            stitcherBuilder.Rotation = int.Parse(argument.Substring(3));
+                            break;
+                        case 'o':
+                            stitcherBuilder.Origin = argument.Substring(3);
+                            break;
+                        case 'c':
+                            stitcherBuilder.Recursive = bool.Parse(argument.Substring(3));
+                            break;
+                        case 'd':
+                            stitcherBuilder.Destination = argument.Substring(3);
+                            break;
+                        case 'n':
+                            stitcherBuilder.Name = argument.Substring(3);
+                            break;
+                        case 'f':
+                            string fileStr = argument.Substring(4);
+                            stitcherBuilder.Files = fileStr.Remove(fileStr.Length - 1, 1).Split(':');
+                            break;
+                        case 'x':
+                            stitcherBuilder.Pattern = argument.Substring(3);
+                            break;
+                        case 's':
+                            stitcherBuilder.Show = bool.Parse(argument.Substring(3));
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+                if (stitcherBuilder.IsValid)
+                {
+                    PdfDocument pdfDocument = stitcherBuilder.Stitch();
+                    if (pdfDocument == null)
+                    {
+                        Console.WriteLine("Returned a null document.");
+                        return;
+                    }
+
+                    pdfDocument.Save(@stitcherBuilder.Destination + "\\" + stitcherBuilder.Name + ".pdf");
+                    Console.WriteLine("\r\nPDF Created");
+
+                    if (stitcherBuilder.Show)
+                    {
+                        try
+                        {
+                            Process.Start("acrobat", @stitcherBuilder.Destination + "\\" + stitcherBuilder.Name + ".pdf");
+                        }
+                        catch (Exception)
+                        {
+                            try
+                            {
+                                Process.Start("AcroRd32", @stitcherBuilder.Destination + "\\" + stitcherBuilder.Name + ".pdf");
+                            }
+                            catch (Exception)
+                            {
+                                try
+                                {
+                                    Process.Start(@stitcherBuilder.Destination + "\\" + stitcherBuilder.Name + ".pdf");
+                                }
+                                catch (Exception)
+                                {
+
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                    Console.WriteLine("Can not complete operation; Type or Name switch is invalid.");
+            }
+        }
+    }
+}
---
+++ PdfStitcher/StitcherBuilder.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using PdfSharp.Pdf;
+
+namespace PdfStitcher
+{
+    internal class StitcherBuilder
+    {
+        internal enum Types
+        {
+            INVALID,
+            PDF,
+            INVENTOR
+        }
+        internal static Types GetType(string type)
+        {
+            if (type.ToLower() == "pdf")
+                return Types.PDF;
+            else if (type.ToLower() == "inventor")
+                return Types.INVENTOR;
+            return Types.INVALID;
+        }
+
+        internal StitcherBuilder()
+        {
+            Type = Types.INVALID;
+            Rotation = 0;
+            Origin = Environment.CurrentDirectory;
+            Recursive = true;
+            Show = true;
+            Destination = Environment.CurrentDirectory;
+            Name = "MERGED_" + DateTime.Now.ToString("yyyyMMddHHmmss");
+            Files = null;
+            Pattern = null;
+        }
+
+        internal Types Type { get; set; }
+        internal int Rotation { get; set; }
+        internal string Origin { get; set; }
+        internal bool Recursive { get; set; }
+        internal string Destination { get; set; }
+        internal string Name { get; set; }
+        internal string[] Files { get; set; }
+        internal string Pattern { get; set; }
+        internal bool Show { get; set; }
+
+        internal bool IsValid
+        {
+            get
+            {
+                if (Type == Types.INVALID)
+                    return false;
+
+                bool fileNameValid = !string.IsNullOrEmpty(Name) && Name.IndexOfAny(Path.GetInvalidFileNameChars()) < 0 && !File.Exists(Path.Combine(Destination, Name));
+                if (!fileNameValid)
+                    return false;
+
+                return true;
+            }
+        }
+
+        internal PdfDocument Stitch()
+        {
+            if (!IsValid)
+                return null;
+            PdfDocument document;
+            if (Type == Types.PDF)
+                StitchPdf(this, out document);
+            else
+                StitchInventor(this, out document);
+
+            return document;
+        }
+
+        private static void StitchDocuments(IEnumerable<string> documents, int rotation, ref PdfDocument pdfDocument)
+        {
+            PdfDocument importPdf;
+            int count = documents.Count();
+            int i = 1;
+            foreach (string document in documents)
+            {
+                using (importPdf = PdfSharp.Pdf.IO.PdfReader.Open(document, PdfSharp.Pdf.IO.PdfDocumentOpenMode.Import))
+                {
+                    foreach (PdfPage page in importPdf.Pages)
+                    {
+                        page.Rotate = rotation % 360;
+                        pdfDocument.AddPage(page);
+                    }
+                    Console.Write("\rStitching PDFs {0} of {1}", i++, count);
+                }
+            }
+        }
+
+        private static void StitchPdf(StitcherBuilder stitcherBuilder, out PdfDocument document)
+        {
+            document = new PdfDocument();
+
+            if ((stitcherBuilder.Files == null || stitcherBuilder.Files.Count() == 0) && stitcherBuilder.Pattern == null) //Get all pdfs in origin directory.
+            {
+                string[] files = Directory.GetFiles(stitcherBuilder.Origin + "\\", "*.pdf", (stitcherBuilder.Recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly));
+
+                StitchDocuments(files, stitcherBuilder.Rotation, ref document);
+
+            }
+            else if (stitcherBuilder.Pattern != null) //Use the regex string to get all needed files
+            {
+                string fileName;
+                string[] files = Directory.GetFiles(stitcherBuilder.Origin + "\\", "*.pdf", (stitcherBuilder.Recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly));
+                List<string> matchedFiles = new List<string>();
+                foreach (string file in files)
+                {
+                    int startIndex = file.LastIndexOf('\\') + 1;
+                    int length = file.Length - startIndex - 4;
+                    fileName = file.Substring(startIndex, length);
+
+                    if (Regex.IsMatch(fileName, stitcherBuilder.Pattern))
+                        matchedFiles.Add(stitcherBuilder.Origin + "\\" + fileName + ".pdf");
+                }
+
+                StitchDocuments(matchedFiles, stitcherBuilder.Rotation, ref document);
+
+            }
+            else //Use the file list
+            {
+                StitchDocuments(stitcherBuilder.Files.Select(s => string.Concat(stitcherBuilder.Origin + "\\" + s + ".pdf")), stitcherBuilder.Rotation, ref document);
+            }
+        }
+
+        private static void StitchInventor(StitcherBuilder stitcherBuilder, out PdfDocument document)
+        {
+            document = new PdfDocument();
+        }
+    }
+}
